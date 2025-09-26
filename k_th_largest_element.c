1f71dfae343262d8dcc087a71324de1011dd52ae
Aaditya Mukherjee <84586322+AadityaMukherjee2007@users.noreply.github.com>
2025-09-26 11:14:47 +0000

    boiler plate code for kth largest element completed

---
+++ k_th_largest_element.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int k_largest(int[], int, int);
+
+int test_arr = {4,5,3,6,7,1,9,8,2};
+int n = 9;
+
+int main()
+{
+    // int n;
+    // printf("Lenght of Array: ");
+    // scanf("%d", &n);
+
+    // int arr[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     printf("arr[%d]: ", i);
+    //     scanf("%d", &arr[i]);
+    // }
+
+    return 0;
+}
+
+int k_largest(int arr[], int n, int k)
+{
+    for (int i = 0; i < n; i++);
+}
