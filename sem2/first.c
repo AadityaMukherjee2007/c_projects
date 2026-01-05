7fd9f54bf613004faaf2657e7f1ac9ed178f6a9f
Aaditya Mukherjee <84586322+AadityaMukherjee2007@users.noreply.github.com>
2026-01-05 10:55:09 +0000

    first class

--- sem2/first.c
+++ sem2/first.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Our first program\n");
+    unsigned int m, n, p;
+    // printf("Our first program\n");
+
+    printf("Enter 2 nums: ");
+    scanf("%u%u", &m, &n);
+
+    p = n + m;
+    printf("Sum: %d\n", p);
+
     return 0;
 }
